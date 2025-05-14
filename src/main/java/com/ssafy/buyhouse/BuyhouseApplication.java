098d27908d500014e62f4103fa3a53a6aaf1339a
김민규 <jun3021303@naver.com>
2025-05-14 14:36:46 +0900

    feat: back 구조 파일 업로드 완료

---
+++ src/main/java/com/ssafy/buyhouse/BuyhouseApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.buyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyhouseApplication.class, args);
+	}
+
+}
